4ee545fbd12bfafa20055339f13dc52f70242ec5
sunaisded <ai.danege@gmail.com>
2017-05-30 18:40:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-
+<?php
+echo file_get_contents('index.txt'); 
+$logfile = 'iplog.data';
+$ip = $_SERVER['REMOTE_ADDR'];
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+$country = (string) $details->country;
+$city = (string) $details->city;
+$region = (string) $details->region;
+date_default_timezone_set('America/New_York');
+$toAdd = "[" . date("M jS, Y - h:ia") . "] - {$country} - {$city} - {$region} - {$ip}";
+$current = file_get_contents($logfile);
+$new = $toAdd . PHP_EOL . $current;
+file_put_contents($logfile, $new);
+?>
